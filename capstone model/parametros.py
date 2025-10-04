edc80bce8efbae73bdbf8be6f83a3024ede27702
sprieb <sprieb@uc.cl>
2025-10-04 15:59:10 -0300

    Add files via upload

---
+++ capstone model/parametros.py
@@ -0,0 +1,10 @@
+Pviaje=[[[1,1,1],[1,1,1]],[[1,1,1],[1,1,1]]] #precio asociado a el viaje con un pasajero ijt
+Creub=[[[1,1,1],[1,1,1]],[[1,1,1],[1,1,1]]] #costo de reubicar un vehiculo ijt
+Tbtw= [[0,1],[1,0]] #tiempo entre nodos
+tmax= 2
+N=2
+A=10
+T=3
+Dem=[[[0,0,0],[1,1,1]],[[1,1,1],[0,0,0]]] #demanda de viajar desde i hasta j en el momento t
+PosI=[[[1,0],[]],[[],[]]]
+
